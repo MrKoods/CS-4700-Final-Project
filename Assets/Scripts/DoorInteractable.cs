b3a3c1de307b67592b2b2e1ec334209308e4be14
insertvoid <isabel.74321@gmail.com>
2026-05-02 17:50:05 -0700

    Added Colliders + New Scenes
    Added colliders to the houses and characters. Added New Scenes that are 'inside the houses' each named 'InsideHouse' (BL - bottom left, R - right, etc).

--- Assets/Scripts/DoorInteractable.cs
+++ Assets/Scripts/DoorInteractable.cs
@@ -8,12 +8,17 @@ public class DoorInteractable : MonoBehaviour
 	private Transform	_playerTransform;
 	public float InteractionRadius = DefaultInteractionRadius;
 
+	[Header("Scene Settings")]
+	public string sceneToLoad;
+
 	[Header("UI")]
 	public GameObject interactPrompt;
+
 
 	void Start()
 	{
 		GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+
 		if (playerObj != null)
 			_playerTransform = playerObj.transform;
 		else
@@ -31,12 +36,16 @@ public class DoorInteractable : MonoBehaviour
 		float distance = Vector3.Distance(transform.position, _playerTransform.position);
 
 		bool inRange = distance <= InteractionRadius;
+
 		if (interactPrompt != null)
 			interactPrompt.SetActive(inRange);
 		
 		if (inRange && Input.GetKeyDown(KeyCode.E))
 		{
-			SceneManager.LoadScene("InsideHouse");
+			if(!string.IsNullOrEmpty(sceneToLoad))
+				SceneManager.LoadScene(sceneToLoad);
+			else
+				Debug.LogWarning("No scene assigned to this door!", this);
 		}
 	}
 }
